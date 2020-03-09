266d7828537e556543ebafe7437cda24a83ae79e
pragmatizt <pragmatizt@users.noreply.github.com>
2020-03-09 16:52:26 -0400

    revision to 02.  03 is confusing

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,14 +9,20 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+import fileinput
+for line in fileinput.input():
+    process(line)
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+print(sys.getwindowsversion())
+
+"""When I run on terminal, this is the output.  Not sure if this is right.
+sys.getwindowsversion(major=10, minor=0, build=18363, platform=2, service_pack='')"""
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
+print(sys.version_info)
 
+"""Also not sure if this is right."""
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
--- src/05_lists.py
+++ src/05_lists.py
@@ -6,12 +6,18 @@ y = [8, 9, 10]
 
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
+"""Look up what Brady did in lecture.  The first thing that comes to mind is the .extend that he went over."""
+
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+another_list = [4]
+x.extend(another_list)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+"""It becomes a list within a list, so that's not right"""
+x.append(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
@@ -24,6 +30,7 @@ print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
