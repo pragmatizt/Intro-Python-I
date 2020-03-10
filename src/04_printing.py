a5f12269781ec0442eb2e1ff63dd9e0964351178
pragmatizt <pragmatizt@users.noreply.github.com>
2020-03-10 14:51:00 -0400

    #4 & #6 done

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,18 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print("x is", x, ", y is", '%.2f' % y, ", z is", '"'+z+'"')
+
+"""Interesting, so I'll need to read more about what the + signs 
+in front of and behind z do."""
 
 # Use the 'format' string method to print the same thing
+print('x is', x, ', y is', "{0:.2f}".format(y), ', z is', '"'+z+'"')
+
+"""So is the biggest value add of the format string the curly braces 
+and the .format()?"""
 
 # Finally, print the same thing using an f-string
+print('x is {}, y is {}, z is "{}"'.format(x, "{0:.2f}".format(y), z))
+
+"""I definitely like this the best -- makes the most intuitive sense, too!"""
--- src/06_tuples.py
+++ src/06_tuples.py
@@ -34,11 +34,17 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # Write a function `print_tuple` that prints all the values in a tuple
 
-# YOUR CODE HERE
+def print_tuple(tuple):
+    for i in tuple:
+        print(i)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
+
+"""If there is one element, one has to write (a,)
+http://doc.sagemath.org/html/en/thematic_tutorials/tutorial-programming-python.html
+"""
